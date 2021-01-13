7e1f9caf9a6762f8ead20565d3df1206eb15ae22
victor.g <victor.g@p2p.org>
2021-01-14 03:00:02 +0500

    fix bug

--- datastore/pg/datastore.go
+++ datastore/pg/datastore.go
@@ -23,7 +23,7 @@ const queryGetAllLostTransactions = `select distinct(b.height) from (select unne
     left join cosmos.transactions t on t.tx_hash = b.tx_hash where t.tx_hash is null order by b.height asc limit 1000`
 
 const queryGetAllLostTransactionsHashes = `select b.tx_hash from (select unnest(txs_hash) as tx_hash, height from cosmos.blocks where num_tx > 0 order by height limit 100000) as b
-    left join cosmos.transactions t on t.tx_hash = b.tx_hash where t.tx_hash is null order by b.height asc limit 1000`
+    left join cosmos.transactions t on t.tx_hash = b.tx_hash where t.tx_hash is null order by b.height asc limit 10`
 
 const queryBlocksWithCountTxs = `SELECT b.height, b.num_tx, count(t.block_height) as count_txs FROM cosmos.blocks b
                                                                         left join cosmos.transactions t on t.block_height = b.height and t.chain_id = b.chain_id
